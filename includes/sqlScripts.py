c06e219bb7a8dbd05057066ae060cb28932e602a
michaelmagdyshaker <michael.magdyy96@gmail.com>
2022-07-23 19:41:55 +0200

    Add files via upload

--- includes/sqlScripts.py
+++ includes/sqlScripts.py
@@ -1,8 +1,13 @@
+##########################################################################################################################################################################
+'''
+                      This is scripts file which called from the server and worker side, where the queries are stored
+'''
+##########################################################################################################################################################################
 class scripts:  
-    
+    # drop Events table
     drop_Events_table_if_exist = ''' DROP TABLE IF EXISTS Events;'''
 
-   
+   # create Events table
     create_Events_table_query = '''
                                 CREATE TABLE IF NOT EXISTS Events (
                                     id INTEGER PRIMARY KEY,
@@ -11,10 +16,10 @@ class scripts:
                                 '''
 
    
-   
+    # drop Repos table
     drop_Repos_table_if_exist = ''' DROP TABLE IF EXISTS Repos;'''
 
-  
+    # create Repos table
     create_Repos_table_query = '''
                                 CREATE TABLE IF NOT EXISTS Repos (
                                     id INTEGER PRIMARY KEY,
@@ -24,10 +29,10 @@ class scripts:
                                 '''
                             
    
-   
+    # drop PullRequests table   
     drop_PullRequests_table_if_exist = ''' DROP TABLE IF EXISTS PullRequests;'''
 
-   
+   # create PullRequests table
     create_PullRequests_table_query = '''
                                 CREATE TABLE IF NOT EXISTS PullRequests (
                                     id INTEGER PRIMARY KEY,
@@ -48,49 +53,56 @@ class scripts:
                                 '''
 
   
-  
-    get_avg_pr_time_using_id = '''
-                                SELECT AVG(CAST ( (JulianDay(next_created_at) - JulianDay(created_at) ) * 24 * 60 * 60 AS INTEGER) ) 
-                                FROM (
-                                        SELECT created_at,
-                                               LEAD(created_at, 1, created_at) OVER (ORDER BY created_at) AS next_created_at
-                                            FROM PullRequests
-                                            WHERE id = @ID
-                                    );
-                                '''                               
+    # get the average time between a specific repo pr using id 
+    get_avg_pr_time_using_repo_param = '''
+                                        SELECT AVG(CAST ( (JulianDay(next_created_at) - JulianDay(created_at) ) * 24 * 60 AS INTEGER) ) 
+                                        FROM (
+                                                SELECT pr.created_at,
+                                                        LEAD(pr.created_at, 1, pr.created_at) OVER (ORDER BY pr.created_at) AS next_created_at
+                                                    FROM PullRequests pr
+                                                    INNER JOIN Repos r 
+                                                        ON r.event_id = pr.event_id
+                                                    WHERE r.id = '@PARAM' OR 
+                                                        INSTR(r.name, '@PARAM') > 0 OR 
+                                                        INSTR(r.url, '@PARAM') > 0
+                                            );                                
+                                        '''                               
 
-     
-    get_avg_repo_time_using_freetxt = '''
-                                SELECT AVG(CAST ( (JulianDay(next_created_at) - JulianDay(created_at) ) * 24 * 60 * 60 AS INTEGER) ) 
-                                FROM (
-                                        SELECT created_at,
-                                                LEAD(created_at, 1, created_at) OVER (ORDER BY created_at) AS next_created_at
-                                            FROM PullRequests
-                                            WHERE INSTR(url, '@VAR') > 0 OR 
-                                                INSTR(node_id, '@VAR') > 0 OR 
-                                                INSTR(html_url, '@VAR') > 0 OR 
-                                                INSTR(diff_url, '@VAR') > 0 OR 
-                                                INSTR(patch_url, '@VAR') > 0 OR 
-                                                INSTR(issue_url, '@VAR') > 0 OR 
-                                                INSTR(title, '@VAR') > 0 OR 
-                                                INSTR(body, '@VAR') > 0 OR 
-                                                INSTR(commits_url, '@VAR') > 0
-                                    );
-
-                                '''                               
-  
-    get_avg_repo_time_using_number = '''
-                                SELECT AVG(CAST ( (JulianDay(next_created_at) - JulianDay(created_at) ) * 24 * 60 * 60 AS INTEGER) ) 
-                                FROM (
-                                        SELECT created_at,
-                                               LEAD(created_at, 1, created_at) OVER (ORDER BY created_at) AS next_created_at
-                                            FROM PullRequests
-                                            WHERE number = @NUM
-                                    );
-                                '''
+    # get max change time for specific repo or pr with a given param 
+    get_max_pr_time_on_repo_using_pr_or_repo_param = '''
+                                    SELECT max(pr.created_at) 
+                                    FROM PullRequests pr
+                                        INNER JOIN
+                                        Repos r ON r.event_id = pr.event_id
+                                    WHERE pr.id = '@VAR' OR 
+                                        r.id = '@VAR' OR 
+                                        INSTR(pr.url, '@VAR') > 0 OR 
+                                        INSTR(node_id, '@VAR') > 0 OR 
+                                        INSTR(html_url, '@VAR') > 0 OR 
+                                        INSTR(diff_url, '@VAR') > 0 OR 
+                                        INSTR(patch_url, '@VAR') > 0 OR 
+                                        INSTR(issue_url, '@VAR') > 0 OR 
+                                        INSTR(title, '@VAR') > 0 OR 
+                                        INSTR(body, '@VAR') > 0 OR 
+                                        INSTR(commits_url, '@VAR') > 0 OR 
+                                        INSTR(r.name, '@VAR') > 0 OR 
+                                        INSTR(r.url, '@VAR') > 0;
+                                    '''                               
 
 
+  # get all changes of prs happened on a speicic repo over time
+    get_changes_over_time_for_specific_repo = '''
+                                    SELECT e.created_at
+                                    FROM Repos r
+                                        INNER JOIN
+                                        events e ON e.id = r.event_id
+                                    WHERE r.id = '@VAR' OR 
+                                        INSTR(r.name, '@VAR') > 0 OR 
+                                        INSTR(r.url, '@VAR') > 0;
+                                    '''
 
+
+    # get count for each event tybe in db
     get_grouped_events = '''
                             SELECT type,
                                 count(1) 
